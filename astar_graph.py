d8c52dcd45db5c844bfa2a6ab98e1ae06a482aa0
Adam Černý <adamcerny13@icloud.com>
2021-04-19 21:11:00 +0200

    xd

--- astar_graph.py
+++ astar_graph.py
@@ -19,14 +19,7 @@ class Astar:
 
 	def heurestic(self, n):
 		nodes = {
-				"A" : 1,
-				"B" : 1,
-				"C" : 1,
-				"D" : 1,
-				"E" : 1,
-				"F" : 1,
-				"G" : 1,
-				"H" : 1
+				key: 1 for key in self.nodes.keys()
 				}
 
 		return nodes[n]
