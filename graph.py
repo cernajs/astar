1b5ea9114fb20b9cc04218a0d4c8b4a996c5b0c5
Adam Černý <adamcerny13@icloud.com>
2021-04-22 19:17:21 +0200

    semifinal

--- graph.py
+++ graph.py
@@ -1,13 +1,7 @@
-import timeit
-import string
-import matplotlib.pyplot as plt
+
 import networkx as nx
 from itertools import combinations
-from random import random, randint
-
-
-from astar_graph import  Astar
-
+from random import random
 
 """
 nodes = {
@@ -37,41 +31,3 @@ def ER(n, p):
     return g
 
 
-n = 50
-p = 0.1
-G = ER(n, p)
-a = nx.convert.to_dict_of_lists(G)
-
-
-dict_values = a.items()
-
-new_a = {str(key):value for key, value in dict_values} 
-
-
-dict_values2 = new_a.items()
-
-for key, value in dict_values2:
-    new_a[key] = [(str(v),random.randint(1,3)) for v in value]
-    
-
-astar = Astar(new_a,"0", "48")
-print(astar.search())
-
-shortest_path = [int(i) for i in astar.search()]
-print(shortest_path)
-node_colors = ["blue" if n in shortest_path else "red" for n in G.nodes()]
-
-
-pos = nx.spring_layout(G)
-nx.draw_networkx(G, pos, node_color=node_colors)
-
-
-for i in range(0,len(shortest_path)):
-    if i+1 == len(shortest_path):
-        break
-    nx.draw_networkx_edges(G,pos,
-    edgelist=[(shortest_path[i],shortest_path[i+1])],
-    edge_color="blue",width=2)
-
-    
-plt.show()
